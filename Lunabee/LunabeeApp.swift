93d6aa96d245ceba7776008a5e55ea15e8f0c1ba
mercvre <jordan_louis@outlook.com>
2023-02-18 13:55:51 +0100

    Initial Commit

---
+++ Lunabee/LunabeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunabeeApp.swift
+//  Lunabee
+//
+//  Created by Heesham on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LunabeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
